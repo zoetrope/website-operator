1d709e6f5ce48418400622c2edec751c8191363a
zoetrope <a.ikezoe@gmail.com>
2020-11-02 19:01:45 +0900

    Remove elasticsearch from e2e-test

--- e2e/bootstrap_test.go
+++ e2e/bootstrap_test.go
@@ -5,11 +5,10 @@ import (
 	"net/http"
 	"time"
 
-	appsv1 "k8s.io/api/apps/v1"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	websitev1beta1 "github.com/zoetrope/website-operator/api/v1beta1"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 )
 
