ef08571baa6d3dcbe3cd3d5a2cb2a037bbfa477c
zoetrope <a.ikezoe@gmail.com>
2020-11-03 16:32:35 +0900

    Add tests for mkdocs and gatsby

--- e2e/bootstrap_test.go
+++ e2e/bootstrap_test.go
@@ -2,6 +2,7 @@ package e2e
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -12,38 +13,50 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
+func testWebSite(name string) {
+	var site websitev1beta1.WebSite
+	Eventually(func() error {
+		err := getResource("default", "website", name, "", &site)
+		if err != nil {
+			return err
+		}
+		if site.Status.Ready != corev1.ConditionTrue {
+			return fmt.Errorf("%s should be ready", name)
+		}
+		return nil
+	}, 1*time.Minute).Should(Succeed())
+
+	var deployment appsv1.Deployment
+	Eventually(func() error {
+		err := getResource("default", "deployment", name, "", &deployment)
+		if err != nil {
+			return err
+		}
+		if deployment.Status.AvailableReplicas != 2 {
+			return errors.New("should be ready")
+		}
+		return nil
+	}, 3*time.Minute).Should(Succeed())
+
+	req, err := http.NewRequest(http.MethodGet, "http://127.0.0.1", nil)
+	Expect(err).ShouldNot(HaveOccurred())
+	req.Host = name + ".default.example.com"
+	client := http.Client{}
+	res, err := client.Do(req)
+	Expect(err).ShouldNot(HaveOccurred())
+	Expect(res.StatusCode).Should(Equal(http.StatusOK))
+}
+
 func testBootstrap() {
 	It("should launch honkit-sample", func() {
-		var site websitev1beta1.WebSite
-		Eventually(func() error {
-			err := getResource("default", "website", "honkit-sample", "", &site)
-			if err != nil {
-				return err
-			}
-			if site.Status.Ready != corev1.ConditionTrue {
-				return errors.New("honkit-sample should be ready")
-			}
-			return nil
-		}, 1*time.Minute).Should(Succeed())
+		testWebSite("honkit-sample")
+	})
 
-		var deployment appsv1.Deployment
-		Eventually(func() error {
-			err := getResource("default", "deployment", "honkit-sample", "", &deployment)
-			if err != nil {
-				return err
-			}
-			if deployment.Status.AvailableReplicas != 2 {
-				return errors.New("should be ready")
-			}
-			return nil
-		}, 3*time.Minute).Should(Succeed())
+	It("should launch mkdocs-sample", func() {
+		testWebSite("mkdocs-sample")
+	})
 
-		req, err := http.NewRequest(http.MethodGet, "http://127.0.0.1", nil)
-		Expect(err).ShouldNot(HaveOccurred())
-		req.Host = "honkit-sample.default.example.com"
-		client := http.Client{}
-		res, err := client.Do(req)
-		Expect(err).ShouldNot(HaveOccurred())
-		Expect(res.StatusCode).Should(Equal(http.StatusOK))
+	It("should launch gatsby-sample", func() {
+		testWebSite("gatsby-sample")
 	})
 }
